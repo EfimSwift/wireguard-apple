25b886c9f1fc7b1353f17624c84d8dd907b6ab02
EfimSwift <a01871893@gmail.com>
2025-08-03 21:49:48 +0300

    Add placeholder wireguard.go for WireGuardGoBridge

---
+++ Sources/WireGuardKitGo/wireguard.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "golang.zx2c4.com/wireguard/wgctrl"
+    "golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+)
+
+// Dummy function to satisfy build requirements
+func main() {
+    // Placeholder for WireGuard Go bridge
+}
+
